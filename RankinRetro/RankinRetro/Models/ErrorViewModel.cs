20675a71605599e52d1febce7c0856bbf48ce660
lAzico <chris.rankin90@googlemail.com>
2023-10-22 23:16:25 +0100

    Adding the project files

---
+++ RankinRetro/RankinRetro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RankinRetro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
